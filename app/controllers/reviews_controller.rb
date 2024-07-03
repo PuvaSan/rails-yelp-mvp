ea8a047b7ce2909ce695a13fb6b096b3fdda7d8d
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-07-03 17:19:33 +0900

    no issues with all user stories, fully functional, next up is bootstrap classes to make it look nice

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,30 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+
+    if @review.save
+      redirect_to @restaurant, notice: 'Review was successfully made'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.required(:review).permit(:rating, :content)
+  end
+
 end
